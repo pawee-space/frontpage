d044212935b8c58bdfd9d6065e91150a5a107c14
pmqueiroz <pedroqueiroz593@gmail.com>
2020-10-05 14:22:58 -0300

    chore: set up project

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Dashboard() {
+  return (
+    <div >
+      <Head>
+        <title>Pawee Space - Dashboard</title>
+      </Head>
+      <h1>Pawee Space</h1>
+    </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Pawee Space</title>
+      </Head>
+      <h1>Pawee Space</h1>
+    </div>
+  )
+}
