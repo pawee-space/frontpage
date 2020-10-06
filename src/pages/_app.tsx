ff6e3f42f07be72581a603d61c7cdbecd8d7fa6f
pmqueiroz <pedroqueiroz593@gmail.com>
2020-10-06 15:07:48 -0300

    refactor: some linting changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/global';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
