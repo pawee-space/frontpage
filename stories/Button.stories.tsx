cd589a032a0ee59206a95f4e5c9b445e5638aabe
pmqueiroz <pedroqueiroz593@gmail.com>
2020-12-01 12:39:53 -0300

    docs: update buttom component

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -5,7 +5,7 @@ import { Story, Meta } from '@storybook/react/types-6-0';
 import { Button, ButtonProps } from './Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'Components/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -22,17 +22,5 @@ Primary.args = {
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
   label: 'Button',
 };
--- stories/Button.tsx
+++ stories/Button.tsx
@@ -2,34 +2,14 @@ import React from 'react';
 import './button.css';
 
 export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button: React.FC<ButtonProps> = ({
   primary = false,
-  size = 'medium',
   backgroundColor,
   label,
   ...props
@@ -38,7 +18,7 @@ export const Button: React.FC<ButtonProps> = ({
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', 'storybook-button--medium', mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
