f0b2c722ea78d665f6cfc731ca9c82fbe8e2f795
pmqueiroz <pedroqueiroz593@gmail.com>
2020-12-18 10:01:34 -0300

    chore: add toast to profile

--- src/components/CreatePost/index.tsx
+++ src/components/CreatePost/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@components/Button';
+import { useToast } from '@hooks/toast';
 import { Container } from './styles';
 
 interface IPost {
@@ -8,18 +9,29 @@ interface IPost {
    postId: string;
 }
 
-const CreatePost: React.FC = () => (
-  <Container>
-    <form className="content">
-      <div className="avatar" />
-      <input type="text" placeholder="Faça alguma publicação!!" />
-    </form>
-    <Button isPrimary>
-      Publicar
-    </Button>
-    {/* <div className="image" /> */}
+const CreatePost: React.FC = () => {
+  const { addToast } = useToast();
 
-  </Container>
-);
+  return (
+    <Container>
+      <form className="content">
+        <div className="avatar" />
+        <input type="text" placeholder="Faça alguma publicação!!" />
+      </form>
+      <Button
+        isPrimary
+        onClick={() => addToast({
+          type: 'error',
+          title: 'Oops',
+          description: 'Ainda não é possível criar posts :c',
+        })}
+      >
+        Publicar
+      </Button>
+      {/* <div className="image" /> */}
+
+    </Container>
+  );
+};
 
 export default CreatePost;
